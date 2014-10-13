57ee3b1ce84847d9276b82b174f7691e62e86134
Sasa7 <sasa.rogic@gmail.com>
2014-10-13 12:42:07 +0200

    Initial commit

---
+++ JsonExample/JsonExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using JsonExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsonExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Product> products = new List<Product>{ 
+            new Product{ Id = 1, Name = "Computer", Description="Use to make programms"}, 
+            new Product{Id = 2, Name = "Xbox", Description="Use to play games"}};
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetByJson()
+        {
+            List<int> myList = new List<int> { 5, 6, 7, 8, 9, 10 };
+            return Json(myList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ JsonExample/JsonExample/Scripts/MyScript.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="jquery-2.1.0-vsdoc.js" />
+
+$(function () {
+    $("#myButton").click(function () {
+        var div = $("#results");
+        $.ajax({
+            type: "GET",
+            url: "Home/GetByJson",
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                //$.each(data, function(i, item){
+                //    var res = item.Name + " " + item.Description + "<br /> ";
+                //    div.append(res);
+
+                    $.each(data, function(i, item){
+                        var res = item + "<br /> ";
+                        div.append(res);
+                });
+            }
+        });
+    });
+});
